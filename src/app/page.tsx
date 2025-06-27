0c3e23eade3c9786f209464da4be5d89b0a81d7a
tanbiralam <itzmetanbir@gmail.com>
2025-06-27 16:06:32 +0530

    Initial Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <Button variant="destructive">Click Me</Button>
+    </div>
+  );
+};
+
+export default Page;
